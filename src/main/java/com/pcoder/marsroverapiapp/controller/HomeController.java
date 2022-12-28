3cd0a74344654c03d614af8ba62d6fc1eda771b2
Phuoc Ho <vanphuoc3012@gmail.com>
2022-12-28 10:06:02 +0700

    commit 2

--- src/main/java/com/pcoder/marsroverapiapp/controller/HomeController.java
+++ src/main/java/com/pcoder/marsroverapiapp/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.pcoder.marsroverapiapp.controller;
 
 import com.pcoder.marsroverapiapp.dto.FormDTO;
+import com.pcoder.marsroverapiapp.entity.MarsCamera;
+import com.pcoder.marsroverapiapp.entity.MarsPhoto;
 import com.pcoder.marsroverapiapp.entity.MarsRoverApiResponse;
 import com.pcoder.marsroverapiapp.service.MarsRoverApiService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -21,11 +27,18 @@ public class HomeController {
         if(formDTO.getRoverName() == null) formDTO.setRoverName("curiosity");
         if(formDTO.getSol() == null || formDTO.getSol() == 0) formDTO.setSol(1);
 
+        List<MarsCamera> listMarsCamera = service.cameraList();
+        model.put("listMarsCamera", listMarsCamera);
+
+        if(formDTO.getCameras().isEmpty()) formDTO.setCameras(listMarsCamera.stream().map(marsCamera -> marsCamera.getName()).collect(Collectors.toList()));
 
         model.put("formDTO", formDTO);
 
-        MarsRoverApiResponse roverData = service.getRoverData(formDTO.getRoverName(), formDTO.getSol());
-        model.put("roverData", roverData.getPhotos());
+        Set<MarsPhoto> roverData = service.getRoverData(formDTO);
+        model.put("roverData", roverData);
+
+        if(roverData.isEmpty()) model.put("message", "No photos found");
+
         return "home";
     }
 }
--- src/main/java/com/pcoder/marsroverapiapp/service/MarsRoverApiService.java
+++ src/main/java/com/pcoder/marsroverapiapp/service/MarsRoverApiService.java
@@ -1,18 +1,43 @@
 package com.pcoder.marsroverapiapp.service;
 
+import com.pcoder.marsroverapiapp.dto.FormDTO;
+import com.pcoder.marsroverapiapp.entity.MarsCamera;
+import com.pcoder.marsroverapiapp.entity.MarsPhoto;
 import com.pcoder.marsroverapiapp.entity.MarsRoverApiResponse;
+import com.pcoder.marsroverapiapp.repository.MarsCameraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class MarsRoverApiService {
     private static final String API_KEY = "H9jPVMMWjV1MjSTuWVFcedK0Y5hegoyBumV67NuL";
 
-    public MarsRoverApiResponse getRoverData(String rover, Integer sol) {
+    @Autowired
+    private MarsCameraRepository marsCameraRepository;
+
+    public Set<MarsPhoto> getRoverData(FormDTO formDTO) {
         RestTemplate rt = new RestTemplate();
 
+        String rover = formDTO.getRoverName();
+        int sol = formDTO.getSol();
+
         ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/"+rover+"/photos?sol="+sol+"&api_key="+API_KEY,MarsRoverApiResponse.class);
-        return response.getBody();
+
+        List<MarsPhoto> photos = response.getBody().getPhotos();
+        Set<MarsPhoto> collectPhotos = photos.stream()
+                .filter(marsPhoto -> formDTO.getCameras().contains(marsPhoto.getCamera().getName()))
+                .collect(Collectors.toSet());
+
+        return collectPhotos;
+    }
+
+    public List<MarsCamera> cameraList() {
+        return (List<MarsCamera>) marsCameraRepository.findAll();
     }
 }
